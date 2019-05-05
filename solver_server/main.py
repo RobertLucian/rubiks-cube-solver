18d31b50199da38dcd978e4270337306b922737e
Robert Lucian Chiriac <robert.lucian.chiriac@gmail.com>
2019-05-05 23:29:30 +0300

    chore(code): add boiler plate code #1

---
+++ solver_server/main.py
@@ -0,0 +1,50 @@
+import asyncio
+import json
+import arms
+import kociemba
+
+# to calibrate the arms 
+# and report the identified colors 
+# along with the captured images
+async def configurator():
+    pass
+
+# to manipulate/solve/stop the actual cube
+async def runner():
+    pass
+
+# to broadcast the camera feed
+async def camera():
+    pass
+
+# to collect the images for each face of the cube
+def image_collector():
+    pass
+
+# to detect the colors on each face
+def image_processor():
+    pass
+
+# to solve the cube with the Kociemba algorithm
+def rubik_solver():
+    pass
+
+# to convert the output from the solver to 
+# what the arm generator needs
+def solver_converter():
+    pass
+
+# to convert the generated solution
+# to whatever the arms need
+def arm_generator():
+    pass
+
+# to apply the solution onto the real-world cube
+def cube_executor():
+    pass
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
